53c94a93dfa263d51f7efacff61059a38daa883e
jcarp19 <jim@carpenteronline.us>
2021-09-23 19:45:32 -0400

    details are working

--- src/components/SingleMovieInfo.tsx
+++ src/components/SingleMovieInfo.tsx
@@ -6,10 +6,11 @@ import { Link } from 'react-router-dom';
 import {GenresEntity} from "../models/SingleMovieDetail";
 import "./MovieResults.css";
 import "./MovieDetail.css";
+import "./SingleMovieInfo.css";
+
 
 export const SingleMovieInfo = () => {
     const [singleMovie, setSingleMovie] = useState<SingleMovieDetail>();
-    const [genre, setGenre] = useState<GenresEntity[]>();
 
     const {id}: {id: string} = useParams();
     // console.log(`the params is ${id}`)
@@ -18,17 +19,17 @@ export const SingleMovieInfo = () => {
         .then((data) => setSingleMovie(data))
         // .then((data) => setGenre(data))
         // .then((genres) => setGenre(genres))
-        // .then((data.genres) => setGenre(data.genres))
-            
+        // .then((data) => setGenre(data.genres))
     });
-    
+    // let json_data = {genre}
 
     
     let posterPath = `https://image.tmdb.org/t/p/w500${singleMovie?.poster_path}`;
-    let homePage = `${singleMovie?.homepage}`;
+    
+    
     return (
         <div>
-            <h2>This is the real details page!</h2>
+            <h2 className="page-heading"></h2>
             <div className="movie-results">
                 <img className="moviePoster" src={posterPath} alt={singleMovie?.title} />
                 <div className="movie-results-info">
@@ -38,12 +39,13 @@ export const SingleMovieInfo = () => {
             </div>
             <div className="movie-detail">
                 <p>{singleMovie?.overview}</p>
-                <p><Link to={homePage} className="site-link">Visit Website</Link></p>
-                <p>Popularity: {singleMovie?.popularity}</p>
+                <p><a href={singleMovie?.homepage} className="site-link">Visit Website</a></p>
+                <p>Popularity: {singleMovie?.popularity}(Higher is better)</p>
                 <p>Minutes: {singleMovie?.runtime}</p>
-                <h3>Genre</h3>
-                {genre?.map((genre, index) =>
-                    <p key={index}>{genre.name}</p>
+                <h3 className="genre-heading">Genre</h3>
+                {/* {genre} */}
+                {singleMovie?.genres?.map((genre, index) =>
+                    <p className="genre-name" key={index}>{genre.name}</p>
                 )}
             </div>
             
