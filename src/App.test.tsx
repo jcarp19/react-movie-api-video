26002ce7f1c78cabf48421e7cd4f499b0416d7c8
Areddy28 <reddyanurag28@gmail.com>
2021-09-28 15:11:48 -0400

    Edited Testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,33 +5,33 @@ import SingleMovieInfo from './components/SingleMovieInfo';
 import { Watchlist } from './components/Watchlist';
 import MovieSearchForm from './components/MovieSearchForm';
 
-test('renders learn react link', () => {
-  render(<Watchlist />);
-  const button = screen.getByRole("button", {name: "addMovie"});
-  expect(button).toBeInTheDocument();
+test('Text that displays search for title or filters movies', () => {
+  render(<MovieSearchForm />);
+  const para = screen.getByRole("p", {name: "addp"});
+  expect(para).toBeInTheDocument();
 });
-test('renders learn react link', () => {
-  render(<Watchlist />);
-  const input = screen.getByRole("Input", {name: "addInput"});
+test('Text that displays heading find a great movie', () => {
+  render(<MovieSearchForm />);
+  const input = screen.getByRole("h1", {name: "addh1"});
   expect(input).toBeInTheDocument();
 });
-test('renders learn react link', () => {
-  render(<Watchlist />);
-  const input1 = screen.getByRole("Input1", {name: "addInput1"});
+test('Selects for filtering movies', () => {
+  render(<MovieSearchForm />);
+  const input1 = screen.getByRole("select", {name: "addselect"});
   expect(input1).toBeInTheDocument();
 });
-test('renders learn react link', () => {
+test('Button that searchs for the movies', () => {
   render(<MovieSearchForm />);
   const search = screen.getByRole("Search", {name: "addSearch"});
   expect(search).toBeInTheDocument();
 });
-test('renders learn react link', () => {
+test('Div that holds the search wrapper', () => {
   render(<MovieSearchForm />);
   const div = screen.getByRole("Div", {name: "addDiv"});
   expect(div).toBeInTheDocument();
 });
-test('renders learn react link', () => {
-  render(<Watchlist />);
-  const div1 = screen.getByRole("Div1", {name: "addDiv1"});
+test('Input that is the place holder for search to add text', () => {
+  render(<MovieSearchForm />);
+  const div1 = screen.getByRole("input", {name: "addinput"});
   expect(div1).toBeInTheDocument();
 });
--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -25,28 +25,16 @@ export const Watchlist = () => {
             className="watchlist-container">
                 {watchList.map((item, index) =>
                 <div className="watchlist-card">
-                <div
-                aria-label = "addDiv1"
-                role = "Div1"
-                 className="movie-results" key={index}>
+                <div className="movie-results" key={index}>
                     <img className="moviePoster" src={item.posterPath} />
                     <div className="movie-results-info">
                         <h2 className="movie-title">{item.title}</h2>
-                        <p 
-                        aria-label = "addInput"
-                        role = "Input"
-                        className="movie-runtime">Runtime: {item.runtime} minutes</p>
+                        <p className="movie-runtime">Runtime: {item.runtime} minutes</p>
                     </div>
                 </div>
                     <div className="movie-detail">
-                        <p
-                        aria-label = "addInput1"
-                        role = "Input1"
-                        >{item.plot}</p>
-                        <button 
-                        aria-label = "addMovie"
-                        role = "button"
-                        className="btn" onClick={(e) => {
+                        <p >{item.plot}</p>
+                        <button className="btn" onClick={(e) => {
                             e.preventDefault(); 
                             let newMovieList = [...watchList]
                             newMovieList.splice(index, 1)
