905923c77a146d1391c3eed5cba9ec130a644c29
Areddy28 <reddyanurag28@gmail.com>
2021-09-29 00:08:11 -0400

    Added jest spy fn

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,32 +6,38 @@ import { Watchlist } from './components/Watchlist';
 import MovieSearchForm from './components/MovieSearchForm';
 
 test('Text that displays search for title or filters movies', () => {
-  render(<MovieSearchForm />);
+  const spy = jest.fn();  
+  render(<MovieSearchForm onSubmit = {spy} />);
   const para = screen.getByRole("p", {name: "addp"});
   expect(para).toBeInTheDocument();
 });
 test('Text that displays heading find a great movie', () => {
-  render(<MovieSearchForm />);
+  const spy = jest.fn(); 
+  render(<MovieSearchForm onSubmit = {spy} />);
   const input = screen.getByRole("h1", {name: "addh1"});
   expect(input).toBeInTheDocument();
 });
 test('Selects for filtering movies', () => {
-  render(<MovieSearchForm />);
+  const spy = jest.fn(); 
+  render(<MovieSearchForm onSubmit = {spy} />);
   const input1 = screen.getByRole("select", {name: "addselect"});
   expect(input1).toBeInTheDocument();
 });
 test('Button that searchs for the movies', () => {
-  render(<MovieSearchForm />);
+ const spy = jest.fn();  
+  render(<MovieSearchForm onSubmit = {spy} />);
   const search = screen.getByRole("Search", {name: "addSearch"});
   expect(search).toBeInTheDocument();
 });
 test('Div that holds the search wrapper', () => {
-  render(<MovieSearchForm />);
+  const spy = jest.fn(); 
+  render(<MovieSearchForm onSubmit = {spy} />);
   const div = screen.getByRole("Div", {name: "addDiv"});
   expect(div).toBeInTheDocument();
 });
 test('Input that is the place holder for search to add text', () => {
-  render(<MovieSearchForm />);
+  const spy = jest.fn(); 
+  render(<MovieSearchForm onSubmit = {spy} />);
   const div1 = screen.getByRole("input", {name: "addinput"});
   expect(div1).toBeInTheDocument();
 });
